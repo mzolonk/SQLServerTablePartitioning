1120acc9efb038f70a7ccc9490040e7cc6bac4e8
Nkosi <Mzolonk>
2021-04-09 10:23:27 +0200

    Adding partition setup and a table

---
+++ SQLServerTablePartitioning/Storage/DateAsIntPartPFN.sql
@@ -0,0 +1,18 @@
+﻿CREATE PARTITION FUNCTION [DateAsIntPartPFN](INT)
+    AS RANGE
+    FOR VALUES (
+                 --OffLine Partitions
+                 CONVERT(INT, CONVERT( VARCHAR, GETDATE() - 5, 112)),
+                 CONVERT(INT, CONVERT( VARCHAR, GETDATE() - 4, 112)),
+                 CONVERT(INT, CONVERT( VARCHAR, GETDATE() - 3, 112)),
+                 CONVERT(INT, CONVERT( VARCHAR, GETDATE() - 2, 112)),
+                 CONVERT(INT, CONVERT( VARCHAR, GETDATE() - 1, 112)),
+                 --Today's Partition/ Live partition
+  				 CONVERT(INT, CONVERT( VARCHAR, GETDATE(), 112)),
+                 -- Future Partition
+  				 CONVERT(INT, CONVERT( VARCHAR, GETDATE() + 1, 112)),
+                 CONVERT(INT, CONVERT( VARCHAR, GETDATE() + 2, 112)),
+  				 CONVERT(INT, CONVERT( VARCHAR, GETDATE() + 3, 112)),
+  				 CONVERT(INT, CONVERT( VARCHAR, GETDATE() + 4, 112)),
+  				 CONVERT(INT, CONVERT( VARCHAR, GETDATE() + 5, 112))
+                )
---
+++ SQLServerTablePartitioning/Storage/PartirionFG.sql
@@ -0,0 +1,10 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+  ADD FILEGROUP [PartirionFG];
+GO
+
+ALTER DATABASE [$(DatabaseName)]
+  ADD FILE
+  (
+      NAME = [PartirionFG],
+      FILENAME = '$(DefaultDataPath)$(DefaultFilePrefix)_PartirionFG.ndf'
+  );
